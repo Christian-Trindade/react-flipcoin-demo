8405c15ba3cef4bd1fb706e585e84f51ecf6ff17
Christian <christian.trindade94@gmail.com>
2020-11-23 18:14:32 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FlipCoin from "./components/FlipCoin";
+
+//object options example
+const options = [
+  {
+    name: "Jośe aldo",
+    photo:
+      "https://a.espncdn.com/combiner/i?img=/i/headshots/mma/players/full/2447641.png&w=350&h=254",
+  },
+  {
+    name: "Anderson Silva",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Anderson_Silva.png/200px-Anderson_Silva.png",
+  },
+];
+
+/* Props
+ options: Array with options sides with name and photo
+ winner: Number position of winner on options array
+ winnerMessage: Boolean to show winner message
+ aleatory: Boolean if true the result is aleatory
+*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlipCoin options={options} winner={1} winnerMessage aleatory />
     </div>
   );
 }
